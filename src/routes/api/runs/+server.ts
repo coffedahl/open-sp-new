74fab09d0ed5f8f226b687884bc8d1d28f59e702
coffedahl <coffedahl@hotmail.com>
2023-04-30 11:52:54 +0200

    created api runs route

---
+++ src/routes/api/runs/+server.ts
@@ -0,0 +1,20 @@
+import db from "$lib/database";
+import type { RunRequest } from "$lib/types";
+import type { RequestHandler } from "./$types";
+
+
+export const POST: RequestHandler = async ({ request }) => {
+    const data: RunRequest = await request.json()
+    const res = db.create('run', { store: data.store, type: data.type, date: new Date() })
+    return new Response(JSON.stringify({ res }));
+};
+
+export const GET: RequestHandler = async () => {
+    const data = await db.select('run')
+    return new Response(JSON.stringify({ data: data }))
+}
+
+export const DELETE: RequestHandler = async () => {
+    await db.delete('run')
+    return new Response(JSON.stringify({ message: 'run has been deleted' }))
+}
