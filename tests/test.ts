7f547e074f8ab8b7e81d9465d00f13b24f09c36e
coffedahl <coffedahl@hotmail.com>
2023-04-30 09:34:01 +0200

    Wrote simple ui tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,22 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('Load default page', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Start' })).toBeVisible();
 });
+test.describe('test navigation', () => {
+	test.describe('default button menu', () => {
+		test('price adjustment', async ({ page }) => {
+			await page.goto('/')
+			await page.getByRole('link', { name: 'Price Adjustment' }).click()
+			await expect(page.getByPlaceholder('Price adjustment string')).toBeVisible()
+		})
+	})
+	test.describe('navigation menu', () => {
+		test('test start button', async ({ page }) => {
+			await page.goto('/price-adjustment')
+			await page.getByRole('link', { name: 'Start' }).click()
+			await expect(page.getByRole('heading', { name: 'Start' })).toBeVisible()
+		})
+	})
+})
