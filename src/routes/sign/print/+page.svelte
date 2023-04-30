fa60a7b2431e496c5517083cb0747560c850f1e7
coffedahl <coffedahl@hotmail.com>
2023-04-30 10:42:53 +0200

    Changed the page to use the component

--- src/routes/sign/print/+page.svelte
+++ src/routes/sign/print/+page.svelte
@@ -2,121 +2,18 @@
 	import { onMount } from 'svelte';
 	import type { PageServerData } from './$types';
 	import { goto } from '$app/navigation';
+	import Normal from './normal.svelte';
 
 	export let data: PageServerData;
 	onMount(() => {
 		if (data.print) {
 			window.print();
-			goto('/sign');
+			goto('/sign/edit');
 		}
 	});
 </script>
 
-<div class="flex">
-	<div class="container">
-		<div class="titlecontainer">
-			<h1 id="title">{data.product.title}</h1>
-		</div>
-		<ul>
-			{#each data.product.bullet as bullet}
-				<li>• {bullet}</li>
-			{/each}
-		</ul>
-		<div class="prices">
-			<h1 id="price">{data.product.current.replace(' ', '')}</h1>
-			<h2 id="regular">{data.product.previous.replace(' ', '')}</h2>
-			<p>Art. {data.product.artnr}</p>
-		</div>
-	</div>
-	<div class="container" id="second">
-		<div class="titlecontainer">
-			<h1 id="title">{data.product.title}</h1>
-		</div>
-		<ul>
-			{#each data.product.bullet as bullet}
-				<li>• {bullet}</li>
-			{/each}
-		</ul>
-		<div class="prices">
-			<h1 id="price">{data.product.current.replace(' ', '')}</h1>
-			<h2 id="regular">{data.product.previous.replace(' ', '')}</h2>
-			<p>Art. {data.product.artnr}</p>
-		</div>
-	</div>
+<div class="flex row">
+	<Normal product={data.product} />
+	<Normal product={data.product} />
 </div>
-
-<style>
-	@media print {
-		@page {
-			size: landscape;
-		}
-	}
-	.flex {
-		display: flex;
-		height: 18.5cm;
-		width: 14.85cm;
-	}
-	* {
-		margin: 0;
-		padding: 0;
-	}
-	.container {
-		height: 18.5cm;
-		width: 14.85cm;
-		padding-left: 20px;
-	}
-	#second {
-		padding-left: 90px;
-	}
-	ul {
-		margin-top: 35px;
-		list-style: none;
-	}
-	li {
-		font-size: 28px;
-		font-weight: 400;
-		font-family: 'Lato';
-		margin-top: 3px;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		width: 435px;
-	}
-	.titlecontainer {
-		margin-top: 35px;
-		height: 190px;
-		position: relative;
-		text-overflow: clip;
-		overflow: hidden;
-	}
-	#title {
-		position: absolute;
-		width: 70%;
-		padding-top: 100px;
-		font-size: 50px;
-		font-family: 'Lato';
-		bottom: 0;
-	}
-	.prices {
-		margin-top: 65px;
-	}
-	#price {
-		font-size: 140px;
-		font-weight: 900;
-		font-family: 'Lato';
-		margin-bottom: -0.2em;
-		color: rgb(242, 102, 33);
-	}
-	#regular {
-		font-size: 55px;
-		text-decoration: line-through;
-		font-weight: bold;
-		font-family: 'Lato';
-		margin-left: 10px;
-	}
-	p {
-		font-size: 25px;
-		font-family: 'Lato';
-		margin-left: 15px;
-	}
-</style>
