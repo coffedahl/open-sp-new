fa7361dd899cacdba24da16925c39aae625f1f19
coffedahl <coffedahl@hotmail.com>
2023-04-30 12:03:12 +0200

    Changed cookie line that causes bug for the system

--- src/routes/sign/print/+page.server.ts
+++ src/routes/sign/print/+page.server.ts
@@ -2,7 +2,6 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({ cookies, url }) => {
     const product = JSON.parse(String(cookies.get('product')))
     const print: boolean = Boolean(url.searchParams.get('print')) || false
-    cookies.delete('product')
     return {
         product, print
     }
