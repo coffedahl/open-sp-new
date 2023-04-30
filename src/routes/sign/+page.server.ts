23468e68ea07a1f25ddfacc4f676db73521f439d
coffedahl <coffedahl@hotmail.com>
2023-04-30 11:53:08 +0200

    added run logging to sign  maker

--- src/routes/sign/+page.server.ts
+++ src/routes/sign/+page.server.ts
@@ -14,12 +14,20 @@ export const load: PageServerLoad = async ({ url, cookies }) => {
 };
 
 export const actions: Actions = {
-    default: async function ({ request, cookies }) {
+    default: async function ({ request, cookies, fetch }) {
         const formdata = await request.formData()
+        const store = cookies.get('storenumber')
         const artnr = String(formdata.get('artnr'))
         if (artnr.length == 5) {
             const product = await scrapeProduct(artnr)
             cookies.set('product', JSON.stringify(product))
+            fetch('/api/runs', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ store: store, type: 'sign' })
+            })
             throw redirect(303, '/sign/edit')
         }
     }
