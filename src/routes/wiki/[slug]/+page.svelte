fbff0f1d60189249eab91f034f517c91d3fa24ed
coffedahl <coffedahl@hotmail.com>
2023-05-06 18:18:44 +0200

    Added some margin to bottom of long article

--- src/routes/wiki/[slug]/+page.svelte
+++ src/routes/wiki/[slug]/+page.svelte
@@ -3,12 +3,13 @@
     export let data: PageServerData
 </script>
 
-<article>
+<article class="wiki-article">
     {@html data.article.articleText}
 </article>
 
 <style>
     article {
 		width: 90%;
+        margin-bottom: 8vh;
 	}
 </style>
