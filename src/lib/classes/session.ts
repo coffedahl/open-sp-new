bb0f9f664f8b85bf14a1e04a772d14aa88c8cf4c
coffedahl <coffedahl@hotmail.com>
2023-06-13 00:09:34 +0200

    added country to session class

--- src/lib/classes/session.ts
+++ src/lib/classes/session.ts
@@ -2,22 +2,25 @@ export class Session {
 	private _id: string;
 	private _store: string;
 	private _expires: Date;
+	private _country: 'se' | 'no'
 
 	constructor(id: string, store: string, expires: Date, country: 'se' | 'no') {
 		this._id = id;
 		this._store = store;
 		this._expires = expires;
+		this._country = country
 	}
 
 	static createFromObject(object: any) {
 		if (
 			typeof object.id === 'string' &&
 			typeof object.store === 'string' &&
-			typeof object.expires === 'string'
+			typeof object.expires === 'string' &&
+			(object.country == 'no' || object.country == 'se')
 		) {
 			try {
 				const date = new Date(object.expires);
-				return new Session(object.id, object.store, date, 'no');
+				return new Session(object.id, object.store, date, object.country);
 			} catch {
 				throw Error('The variable "expires" is not datetime: ' + object.expires);
 			}
@@ -48,5 +51,9 @@ export class Session {
 
 	set expires(value: Date) {
 		this._expires = value;
+	}
+
+	get country(): 'se' | 'no' {
+		return this._country
 	}
 }
